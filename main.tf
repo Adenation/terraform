56de3cda63cfe542aa744ce12ac3acee542ee052
Adenation <adenation@hotmail.com>
2023-06-23 10:22:11 +0100

    Updating variables & main
    Updating variables to have the azure resource group name there instead and then referencing the variable rather than the resource group which is no longer be instantiated/imported.

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ resource "random_pet" "rg_name" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = "adentfstorageaccount"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = var.azurerm_resource_group
+  location                 = var.resource_group_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   
@@ -22,28 +22,28 @@ resource "azurerm_storage_container" "gen_storage_container" {
 
 resource "azurerm_data_factory" "adf" {
   name                = "adentfdatafactory"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.azurerm_resource_group
 }
 
 resource "azurerm_databricks_workspace" "databricks" {
   name                = "adentfdatabricks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.azurerm_resource_group
   sku                 = "standard"
 }
 
 resource "azurerm_sql_database" "sqldb" {
   name                = "adentfsqldb"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.resource_group_location
+  resource_group_name = var.azurerm_resource_group
   server_name         = azurerm_mssql_server.sqlserver.name
 }
 
 resource "azurerm_mssql_server" "sqlserver" {
   name                         = "adentfsqlserver"
-  location                     = azurerm_resource_group.rg.location
-  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.resource_group_location
+  resource_group_name          = var.azurerm_resource_group
   version                      = "12.0"
   administrator_login          = "adminuser"
   administrator_login_password = "Password1234!"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
 variable "resource_group_location" {
-  default     = "eastus"
+  default     = "northeurope"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name_prefix" {
   default     = "adentfrg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+}
+
+variable azurerm_resource_group {
+	default = "tf-lab"
+	description = "Name of the resource group
 }
